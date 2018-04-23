8c4cd9601d2b794f626295a3ad79f05d00569e52
macalan <chele@glassbell.com>
2018-04-23 16:28:25 -0400

    update app.js to use PORT not port

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const logger = require('morgan');
 const methodOverride = require('method-override');
 const app = express();
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 //Include our router(s)
 const articlesRouter = require('./routes/articlesRouter');
